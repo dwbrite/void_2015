edda37a2a3f0bed90068a8123a9a0b1dd9004ba0
Julien Cherry <juliencherry@gmail.com>
2017-05-12 10:40:19 -0400

    Add Builder helper class

--- src/Handlers/Text/FormattedString.java
+++ src/Handlers/Text/FormattedString.java
@@ -2,17 +2,63 @@ package Handlers.Text;
 
 import javafx.scene.paint.Color;
 
-// represents a string formatted with some particular format (e.g. color, bold, italics) or animation
-public class FormattedString {
-	CharSequence text;
-	Color color;
-	boolean italics;
-	boolean bold;
-	boolean underline;
-	TextAnimation animation;
+/**
+ * Represents an immutable string formatted with some particular format (e.g. color, bold, italics)
+ * or animation
+ */
+public final class FormattedString {
 
-	public FormattedString(CharSequence text, Color color) {
-		this.text = text;
-		this.color = color;
+	private final CharSequence text;
+	private final Color color;
+	private final boolean italics;
+	private final boolean bold;
+	private final boolean underline;
+	// private final TextAnimation inAnimation;
+	// private final TextAnimation outAnimation;
+
+	public static class Builder {
+
+		private final CharSequence text;
+
+		private Color color = Color.WHITE;
+		private boolean italics = false;
+		private boolean bold = false;
+		private boolean underline = false;
+
+		public Builder(CharSequence text) {
+			this.text = text;
+		}
+
+		public Builder color(Color val) {
+			this.color = val;
+			return this;
+		}
+
+		public Builder italics() {
+			this.italics = true;
+			return this;
+		}
+
+		public Builder bold() {
+			this.bold = true;
+			return this;
+		}
+
+		public Builder underline() {
+			this.underline = true;
+			return this;
+		}
+
+		public FormattedString build() {
+			return new FormattedString(this);
+		}
+	}
+
+	private FormattedString(Builder builder) {
+		this.bold = builder.bold;
+		this.color = builder.color;
+		this.italics = builder.italics;
+		this.text = builder.text;
+		this.underline = builder.underline;
 	}
 }
