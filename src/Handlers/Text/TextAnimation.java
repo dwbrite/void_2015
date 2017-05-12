ff09e5d5e1496e1d82733460617298685cb9c8ff
Julien Cherry <juliencherry@gmail.com>
2017-05-12 17:04:40 -0400

    Change animation

--- src/Handlers/Text/TextAnimation.java
+++ src/Handlers/Text/TextAnimation.java
@@ -1,12 +1,8 @@
 package Handlers.Text;
 
 // represents some text animation
-// todo: separate in and out animations
 public enum TextAnimation {
 	APPEAR,
-	DISAPPEAR,
-	STRIKETHROUGH,
+	NONE,
 	TYPEWRITER,
-	UNTYPE,
-	UNDERLINE
 }
